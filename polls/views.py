ea30cd009782f08349598aebed81d4712b3f0c73
Prakash <99485689+prakash-pg@users.noreply.github.com>
2023-12-21 13:44:44 +0530

    first commit

--- polls/views.py
+++ polls/views.py
@@ -24,22 +24,31 @@ def index_func(request):
                                        'PctNullSelfRedirectHyperlinks','FrequentDomainNameMismatch',
                                        'SubmitInfoToEmail','IframeOrFrame'])
 
-            df2 = {'NumDots': float(NumDots),'PathLevel': float(PathLevel),'NumDash': float(NumDash),
-                   'NumSensitiveWords': float(NumSensitiveWords),'PctExtHyperlinks': float(PctExtHyperlinks)
-                    ,'PctExtResourceUrls': float(PctExtResourceUrls),'InsecureForms': float(InsecureForms),
-                    'PctNullSelfRedirectHyperlinks': float(tooLong),'FrequentDomainNameMismatch':
-                       float(freq),'SubmitInfoToEmail': float(SubmitInfoToEmail),'IframeOrFrame': float(IframeOrFrame)}
+            df2 = pd.DataFrame({
+                'NumDots': float(NumDots) if NumDots else 0.0,
+                'PathLevel': float(PathLevel) if PathLevel else 0.0,
+                'NumDash': float(NumDash) if NumDash else 0.0,
+                'NumSensitiveWords': float(NumSensitiveWords) if NumSensitiveWords else 0.0,
+                'PctExtHyperlinks': float(PctExtHyperlinks) if PctExtHyperlinks else 0.0,
+                'PctExtResourceUrls': float(PctExtResourceUrls) if PctExtResourceUrls else 0.0,
+                'InsecureForms': float(InsecureForms) if InsecureForms.isdigit() else 0.0,
+                'PctNullSelfRedirectHyperlinks': float(tooLong) if tooLong else 0.0,
+                'FrequentDomainNameMismatch': float(freq) if freq.isdigit() else 0.0,
+                'SubmitInfoToEmail': float(SubmitInfoToEmail.lower() == 'yes') if SubmitInfoToEmail else 0.0,
+                'IframeOrFrame': float(IframeOrFrame.lower() == 'yes') if IframeOrFrame else 0.0,
+            }, index=[0])
 
-            df = df.append(df2, ignore_index=True)
-            # load the model from disk
+            df = pd.concat([df, df2], ignore_index=True)
+
+            # Load the model from disk
             filename1 = 'polls/Phishing.pickle'
             loaded_model = pickle.load(open(filename1, 'rb'))
             res = loaded_model.predict(df)
+            
+            # If the model result is 1, set res to True; otherwise, set it to False
+            res = bool(res[0])
+
             print(res)
-            if res[0] == 1:
-                res = True
-            else:
-                res = False
 
         else:
             return redirect('homepage')
